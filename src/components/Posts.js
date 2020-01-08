cadc0723a70af7497eb6373088256cc108e5136a
Luis Osta <luisosta1999@gmail.com>
2020-01-07 18:46:52 -0600

    changed posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import _ from 'lodash';
+import React from "react";
+import _ from "lodash";
 
-import {markdownify, getPages, Link, safePrefix} from '../utils';
+import { markdownify, getPages, Link, safePrefix } from "../utils";
 
 export default class Posts extends React.Component {
-    render() {
-        return (
-            <section id={_.get(this.props, 'section.section_id')} className="wrapper">
-                <div className="inner">
-                    <header className="major">
-                        <h2>{_.get(this.props, 'section.title')}</h2>
-                        {markdownify(_.get(this.props, 'section.subtitle'))}
-                    </header>
-                    <div className="posts">
-                        {_.map(_.orderBy(_.filter(getPages(this.props.pageContext.pages, '/posts'), ['frontmatter.show_in_home_posts', true]), 'frontmatter.date', 'desc'), (post, post_idx) => (
-                            <section key={post_idx} className="post">
-                                {_.get(post, 'frontmatter.alt_img') && 
-                                    <Link to={safePrefix(_.get(post, 'url'))} className="image"><img src={safePrefix(_.get(post, 'frontmatter.alt_img'))} alt="" /></Link>
-                                }
-                                <div className="content">
-                                    <h3>{_.get(post, 'frontmatter.title')}</h3>
-                                    {markdownify(_.get(post, 'frontmatter.excerpt'))}
-                                    <Link to={safePrefix(_.get(post, 'url'))} className="button">More</Link>
-                                </div>
-                            </section>
-                        ))}
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section id={_.get(this.props, "section.section_id")} className="wrapper">
+        <div className="inner">
+          <header className="major">
+            <h2>{_.get(this.props, "section.title")}</h2>
+            {markdownify(_.get(this.props, "section.subtitle"))}
+          </header>
+          <div className="posts">
+            {_.map(
+              _.orderBy(
+                _.filter(getPages(this.props.pageContext.pages, "/posts"), [
+                  "frontmatter.show_in_home_posts",
+                  true
+                ]),
+                "frontmatter.date",
+                "desc"
+              ),
+              (post, post_idx) => (
+                <section key={post_idx} className="post">
+                  {_.get(post, "frontmatter.alt_img") && (
+                    <Link to={safePrefix(_.get(post, "url"))} className="image">
+                      <img
+                        src={safePrefix(_.get(post, "frontmatter.alt_img"))}
+                        alt=""
+                      />
+                    </Link>
+                  )}
+                  <div className="content">
+                    <h3>{_.get(post, "frontmatter.title")}</h3>
+                    {markdownify(_.get(post, "frontmatter.excerpt"))}
+                    <Link
+                      to={safePrefix(_.get("https://www.google.com", "url"))}
+                      className="button"
+                    >
+                      More
+                    </Link>
+                  </div>
+                </section>
+              )
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
