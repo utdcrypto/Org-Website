12a6631960fb867f3107fa38daf6106577f7d0dc
amertx <amerdtx@gmail.com>
2020-02-26 21:42:33 -0600

    added the events page

--- src/templates/events.js
+++ src/templates/events.js
@@ -1,18 +1,36 @@
 import React from "react";
 import _ from "lodash";
+import { Layout, OfficersGrid } from "../components/index";
+import { markdownify } from "../utils";
 
-import { Layout } from "../components/index";
-import { markdownify, Link } from "../utils";
-
-/* eslint-disable */
-
-export default class Elements extends React.Component {
+export default class OurOfficers extends React.Component {
   render() {
-    const title = this.props.pageContext.frontmatter.title;
-    console.log(this.props)
-
-    return <h1>{title}</h1>;
-    
+    const officersToDisplay = this.props.pageContext.frontmatter.officers;
+    console.log(this.props);
+    return (
+      <Layout {...this.props}>
+        <section
+          id="main"
+          className={
+            "wrapper" +
+            (_.get(this.props, "pageContext.frontmatter.sidebar.enabled")
+              ? " sidebar " +
+                _.get(this.props, "pageContext.frontmatter.sidebar.side")
+              : "")
+          }
+        >
+          <div className="inner">
+            <header className="major">
+              <h2>{_.get(this.props, "pageContext.frontmatter.title")}</h2>
+              {markdownify(
+                _.get(this.props, "pageC<ontext.frontmatter.subtitle")
+              )}
+            </header>
+            
+            
+          </div>
+        </section>
+      </Layout>
+    );
   }
-
 }
