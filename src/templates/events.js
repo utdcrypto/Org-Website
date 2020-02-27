967675209a1c469ecc930a6a2148079692b3188d
amertx <amerdtx@gmail.com>
2020-02-26 21:33:45 -0600

    added console.log

--- src/templates/events.js
+++ src/templates/events.js
@@ -9,7 +9,10 @@ import { markdownify, Link } from "../utils";
 export default class Elements extends React.Component {
   render() {
     const title = this.props.pageContext.frontmatter.title;
+    console.log(this.props)
 
     return <h1>{title}</h1>;
+    
   }
+
 }
